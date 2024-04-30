0131ec148bc9b820b2dec2eece8558463945a446
srinithya-r-s <168564102+srinithya-r-s@users.noreply.github.com>
2024-04-30 20:18:07 +0530

    Add files via upload

---
+++ Update/ex.js
@@ -0,0 +1,128 @@
+/*function frt()
+{
+    console.log("Hello");
+    alert("The page is loaded");
+    // javascript code for div burger(first card)
+  /*  var pizza1 = document.createElement("h1");
+    var pizza2 = document.createElement("p");
+    var img = document.createElement("img");
+    img.src="./images/men1.jpg"
+    img.style.width="20%";
+    var pizzaName = document.createElement("h3");
+    var icon1 = document.createElement("i1");
+    icon1.classList.add('fa-regular', 'fa-star');
+    var icon2 = document.createElement("i2");
+    icon2.classList.add('fa-regular', 'fa-star');
+    var icon3 = document.createElement("i3");
+    icon3.classList.add('fa-regular', 'fa-star');
+    var icon4 = document.createElement("i4");
+    icon4.classList.add('fa-regular', 'fa-star');
+    var icon5 = document.createElement("i5");
+    icon5.classList.add('fa-regular', 'fa-star');
+    pizza1.innerHTML = "Awesome Pizza"; 
+    pizza2.innerHTML = "Finished no horrible blessing landlord dwelling dissuade if. Rent fond am he in on read. Anxious cordial demands settled entered in do to colonels"
+    pizzaName.innerHTML = "MICHEL ATHEM";
+    // img.innerHTML = document.createElement("img");
+    document.getElementById("pizza").appendChild(pizza1);
+    document.getElementById("pizza").appendChild(pizza2);
+    document.getElementById("pizza").appendChild(img);
+    document.getElementById("pizza").appendChild(pizzaName);
+    document.getElementById("pizza").appendChild(icon1);
+    document.getElementById("pizza").appendChild(icon2);
+    document.getElementById("pizza").appendChild(icon3);
+    document.getElementById("pizza").appendChild(icon4);
+    document.getElementById("pizza").appendChild(icon5);
+    // Create element:
+//var test = document.createElement("h4");
+//test.innerHTML = "TESTIMONIAL";
+
+// Append to another element:
+//document.getElementById("testimonials").appendChild(test);
+
+// javascript code for div burger(second card)
+var burger1=document.createElement("h1");
+var burger2=document.createElement("p");
+var img = document.createElement("img");
+img.src="./images/men1.jpg"
+img.style.width="20%";
+var burgerName = document.createElement("h3");
+var icon1 = document.createElement("i1");
+icon1.classList.add('fa-regular', 'fa-star');
+var icon2 = document.createElement("i2");
+icon2.classList.add('fa-regular', 'fa-star');
+var icon3 = document.createElement("i3");
+icon3.classList.add('fa-regular', 'fa-star');
+var icon4 = document.createElement("i4");
+icon4.classList.add('fa-regular', 'fa-star');
+var icon5 = document.createElement("i5");
+icon5.classList.add('fa-regular', 'fa-star');
+burger1.innerHTML = "Delicious Burger"; 
+burger2.innerHTML = "Finished no horrible blessing landlord dwelling dissuade if. Rent fond am he in on read. Anxious cordial demands settled entered in do to colonels"
+burgerName.innerHTML = "AHEL JOHN";
+// img.innerHTML = document.createElement("img");
+document.getElementById("burger").appendChild(burger1);
+document.getElementById("burger").appendChild(burger2);
+document.getElementById("burger").appendChild(img);
+document.getElementById("burger").appendChild(burgerName);
+document.getElementById("burger").appendChild(icon1);
+document.getElementById("burger").appendChild(icon2);
+document.getElementById("burger").appendChild(icon3);
+document.getElementById("burger").appendChild(icon4);
+document.getElementById("burger").appendChild(icon5);*/
+
+
+
+/*var array = 
+[{
+    header:"Awesome Pizza",
+    content:"Finished no horrible blessing landlord dwelling dissuade if. Rent fond am he in on read. Anxious cordial demands settled entered in do to colonels",
+    img: "./images/men1.jpg",
+    name:"MICHEL ANTHEM",
+    icon:'class="fa-regular fa-star"'
+
+},
+
+{
+    header:"Awesome Pizza",
+    content:"Finished no horrible blessing landlord dwelling dissuade if. Rent fond am he in on read. Anxious cordial demands settled entered in do to colonels",
+    img: "./images/men1.jpg",
+    name:"MICHEL ANTHEM",
+    icon:'class="fa-regular fa-star"'
+}]
+
+
+  
+ 
+   var i;
+   for (i=0;i<array.length;i++)
+   {
+    /*var a=`<div class="burger" id="burger">
+    <h1>Delicious Burger</h1>
+     <p>"Finished no horrible blessing landlord dwelling dissuade if. Rent fond am he in on read. 
+     Anxious cordial demands settled entered in do to colonel.</p>
+     <img src="./images/men1.jpg" class="men1"><br>
+     <h3>AHEL JOHN</h3>
+     <i class="fa-regular fa-star"></i>
+     <i class="fa-regular fa-star"></i>
+     <i class="fa-regular fa-star"></i>
+     <i class="fa-regular fa-star"></i>
+     <i class="fa-regular fa-star"></i>
+    </div>
+    document.getElementById("card").innerHTML = `<div class="burger" id="burger">
+    <h1>Delicious Burger</h1>
+     <p>"Finished no horrible blessing landlord dwelling dissuade if. Rent fond am he in on read. 
+     Anxious cordial demands settled entered in do to colonel.</p>
+     <img src="./images/men1.jpg" class="men1"><br>
+     <h3>AHEL JOHN</h3>
+     <i class="fa-regular fa-star"></i>
+     <i class="fa-regular fa-star"></i>
+     <i class="fa-regular fa-star"></i>
+     <i class="fa-regular fa-star"></i>
+     <i class="fa-regular fa-star"></i>
+    </div>`;
+
+  
+    console.log("Working");
+   }*/
+
+}
---
+++ Update/s.js
@@ -0,0 +1,3 @@
+document.getElementById("pizza").innerHTML=object1.pizza_content;
+document.getElementById("pizza").innerHTML=object1.pizza_img;
+document.getElementById("pizza").innerHTML=object1.pizza_name;
